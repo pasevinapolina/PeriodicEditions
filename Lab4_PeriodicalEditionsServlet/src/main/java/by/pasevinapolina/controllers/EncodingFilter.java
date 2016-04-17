32f085f221b1adef48d533d0f5c89ef38ab4f950
Polina Pasevina <rigiemp@gmail.com>
2016-04-17 15:49:26 +0300

    encoding filter added

---
+++ Lab4_PeriodicalEditionsServlet/src/main/java/by/pasevinapolina/controllers/EncodingFilter.java
@@ -0,0 +1,35 @@
+package by.pasevinapolina.controllers;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Encoding filter
+ */
+
+public class EncodingFilter implements Filter {
+
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        String encodingRequest = servletRequest.getCharacterEncoding();
+        if(encoding != null && !encoding.equalsIgnoreCase(encodingRequest)) {
+            servletRequest.setCharacterEncoding(encoding);
+            servletResponse.setCharacterEncoding(encoding);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        encoding = null;
+    }
+}
