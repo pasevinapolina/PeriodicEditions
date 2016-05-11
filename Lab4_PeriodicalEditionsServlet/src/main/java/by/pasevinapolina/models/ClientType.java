d7d2617a5bf78c8f59f838b58ce806dea79b6e80
Polina Pasevina <rigiemp@gmail.com>
2016-05-11 20:54:11 +0300

    footer image added

---
+++ Lab4_PeriodicalEditionsServlet/src/main/java/by/pasevinapolina/models/ClientType.java
@@ -0,0 +1,7 @@
+package by.pasevinapolina.models;
+
+/**
+ * Created by user on 08.05.2016.
+ */
+public enum ClientType {
+}
---
+++ Lab4_PeriodicalEditionsServlet/src/main/java/by/pasevinapolina/models/UserRole.java
@@ -0,0 +1,7 @@
+package by.pasevinapolina.models;
+
+/**
+ * Created by user on 03.05.2016.
+ */
+public class UserRole {
+}
